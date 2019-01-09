3e9f3611e8988234b14a1a62a1b0189997fc7b2b
Todd Pike <todd.pike@llamasoft.com>
2019-01-09 09:51:21 -0500

    clojure - completed

--- clojure/prime_factors/src/prime_factors/core.clj
+++ clojure/prime_factors/src/prime_factors/core.clj
@@ -1,4 +1,22 @@
 (ns prime-factors.core)
 
-(defn primes
-  ([num] []))
+;;(defn primes
+;;  ([num]
+;;    (cond
+;;      (< num 2) []
+;;      :else [num])))
+
+; (defn primes
+;   ([num]
+;     (cond
+;       (< num 2) []
+;       (and (zero? (rem num 2)) (> num 2)) [2, (/ num 2)]
+;       :else [num])))
+
+(defn primes 
+  ([num] 
+    (primes num 2 '()))
+  ([num candidate acc]
+    (cond (<= num 1) (reverse acc)
+          (zero? (rem num candidate)) (recur (/ num candidate) candidate (cons candidate acc))
+          :else (recur num (inc candidate) acc))))
