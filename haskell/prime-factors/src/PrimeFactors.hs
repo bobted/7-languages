ca044efaea556560f7ee352f6ff637ab29099066
Todd Pike <todd.pike@llamasoft.com>
2019-01-09 10:47:10 -0500

    haskell - completed

--- haskell/prime-factors/src/PrimeFactors.hs
+++ haskell/prime-factors/src/PrimeFactors.hs
@@ -1,4 +1,12 @@
 module PrimeFactors (primeFactors) where
 
 primeFactors :: Integer -> [Integer]
-primeFactors x = []
+-- primeFactors 1 = []
+-- primeFactors x
+--     | (x `mod` 2 == 0) && (x > 2) = [2] ++ primeFactors (x `div` 2)
+--     | otherwise = [x]
+primeFactors x  = primeFactors' x 2
+    where primeFactors' x c
+            | x < c = []
+            | x `mod` c == 0 = [c] ++ primeFactors' (x `div` c) c
+            | otherwise = primeFactors' x (c + 1)
